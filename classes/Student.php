96bf428df6557ab8d698796d207c73867f4c6016
Samuel <samuelldmj5@gmail.com>
2024-09-21 03:41:36 +0100

    table name corrected

--- classes/Student.php
+++ classes/Student.php
@@ -2,41 +2,43 @@
 
 class Student
 {
-
     public $student_names;
     public $email;
     public $mobile;
 
-
     private $conn;
     private $table_name;
 
-    //constructor
+    // Constructor
     public function __construct($db)
     {
-
         $this->conn = $db;
-        $this->table_name = "tbl_students";
+        $this->table_name = "tbl_student";
     }
 
     public function create_data()
     {
-        $query = "INSERT INTO " . $this->table_name . "SET student_names = ?, email = ?
-        mobile = ? ";
+        // Correct SQL query (added spaces and commas)
+        $query = "INSERT INTO " . $this->table_name . " (student_names, email, mobile) VALUES (?, ?, ?)";
 
-        $obj = $this->conn->prepare($query);
+        // Prepare the query
+        $stmt = $this->conn->prepare($query);
 
-        //sanitizing input variable
+        // Sanitize input
         $this->student_names = htmlspecialchars(strip_tags($this->student_names));
         $this->email = htmlspecialchars(strip_tags($this->email));
         $this->mobile = htmlspecialchars(strip_tags($this->mobile));
 
-        //binding parameter
-        $obj->bind_param("sss", $this->student_names, $this->email, $this->mobile);
+        // Bind parameters using bindValue (or bindParam)
+        $stmt->bindValue(1, $this->student_names);
+        $stmt->bindValue(2, $this->email);
+        $stmt->bindValue(3, $this->mobile);
 
-        if ($obj->execute()) {
+        // Execute the query and check if successful
+        if ($stmt->execute()) {
             return true;
         }
+
         return false;
     }
 }
